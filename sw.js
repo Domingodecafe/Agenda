2eb1f7b1d249d4147568d6d59b68284a2b6ec581
Domingodecafe <domingodecafech@gmail.com>
2026-08-29 10:23:45 -0300

    Corrige encaixe e bordas do modo Dia no mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'agenda-1-0-v8';
-const FILES = ['./','./index.html','./styles.css?v=8','./app.js?v=8','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'agenda-1-0-v9';
+const FILES = ['./','./index.html','./styles.css?v=9','./app.js?v=8','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
