271e229dedb3840cbe3f697a290e6f33b33b57a6
Domingodecafe <domingodecafech@gmail.com>
2026-09-25 10:30:48 -0300

    Reposiciona ações financeiras no mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'agenda-1-0-v17';
-const FILES = ['./','./index.html','./styles.css?v=17','./app.js?v=17','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'agenda-1-0-v18';
+const FILES = ['./','./index.html','./styles.css?v=18','./app.js?v=18','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
