0382d1b889286a6962e3a1479604f66f890a0cf5
Domingodecafe <domingodecafech@gmail.com>
2026-09-25 21:22:25 -0300

    Atualiza Agenda v20: histórico e navegação pela data

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'agenda-1-0-v19';
-const FILES = ['./','./index.html','./styles.css?v=19','./app.js?v=19','./manifest.webmanifest','./icon.svg'];
+const CACHE = 'agenda-1-0-v20';
+const FILES = ['./','./index.html','./styles.css?v=20','./app.js?v=20','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
